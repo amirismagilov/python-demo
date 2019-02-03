1961b61377f39fa3cfae1275b67c9673af2d90e7
amir.ismagilov <Farida@Faridas-MacBook-Air.local>
2019-02-03 11:57:30 +0300

    Rest files

---
+++ main.py
@@ -0,0 +1,8 @@
+from lib import cashback, bmi
+
+print ('our program')
+# value = cashback(10_000) #вызов функции + передача параметра
+# print(value)
+print('кешбэк = ',cashback(10_000))
+print('кешбэк = ',cashback(20_000))
+print('ИМТ = ', bmi(120, 1.8))
