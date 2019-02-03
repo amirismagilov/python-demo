288a537b25debd4bf99065bab4bb3ab48ebd5894
amir.ismagilov <Farida@Faridas-MacBook-Air.local>
2019-02-03 11:42:20 +0300

    Initial commit

---
+++ lib.py
@@ -0,0 +1,20 @@
+def cashback(monthly_expenses, ): #объявление функции
+    """
+    >>> cashback(10_000)
+    300.0
+    """
+    percent = 3
+    result = percent * monthly_expenses / 100
+    return result
+
+def bmi (weight, hight):
+    """
+    >>> bmi(106, 1.68) # doctest: +ELLIPSIS
+    37.55...
+
+    >>> bmi(120, 1.88)
+    33.95201448619285
+    """
+    result = weight / (hight ** 2)
+    return result
+
